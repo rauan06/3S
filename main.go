d0f6818f30d7f2883288b3993b74deed7a0757c9
romirkul <ommirkulr@gmail.com>
2024-10-05 17:57:08 +0500

    Fixed drectory wrong code mistake

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	utils.CheckForHelpAndExit()
 	http.HandleFunc("/", http_utils.Handler)
-	os.Mkdir("buckets", 700)
+	os.Mkdir("buckets", 0o700)
 	address := ":8091"
 	log.Printf("Starting development server at http://localhost%s/\n", address)
 	log.Fatal(http.ListenAndServe(address, nil))
